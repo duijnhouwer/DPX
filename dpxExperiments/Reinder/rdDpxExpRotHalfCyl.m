cf5f7c6f08d58c00b4f8763870f3d0a0afeb15de
reind.dorman@gmail.com <reind.dorman@gmail.com@b6c80056-17eb-79a3-90b5-666747226c3d>
2014-06-11 13:50:53 +0000

    poep

--- dpxExperiments/Reinder/rdDpxExpRotHalfCyl.m
+++ dpxExperiments/Reinder/rdDpxExpRotHalfCyl.m
@@ -69,7 +69,7 @@ for m=1:numel(modes)
             set(R,'kbNames','UpArrow,DownArrow');
             set(R,'correctStimName',fbCorrectStr,'correctEndsTrialAfterSec',10000);
             set(R,'wrongStimName',fbWrongStr,'wrongEndsTrialAfterSec',10000);
-            set(R,'name','resp');
+            set(R,'name','rightHand');
             if dsp<0
                 R.correctKbNames='UpArrow';
             elseif dsp>0
