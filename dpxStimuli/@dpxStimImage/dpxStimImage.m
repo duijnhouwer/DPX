cd463161312cb0be76675a0307cc3bed59268f9b
j.duijnhouwer@gmail.com <j.duijnhouwer@gmail.com@b6c80056-17eb-79a3-90b5-666747226c3d>
2014-07-09 22:59:59 +0000

    dpxStimImage, stub for a stimulus class that displays an image

---
+++ dpxStimuli/@dpxStimImage/dpxStimImage.m
@@ -0,0 +1,23 @@
+classdef dpxStimImage < dpxBasicStim
+    
+    properties (Access=public)
+        RGBA=[0 0 255 255];
+    end
+    properties (Access=protected)
+    end
+    methods (Access=public)
+        function S=dpxStimImage
+        end
+    end
+    methods (Access=protected)
+        function myInit(S)
+        end
+        function myDraw(S)
+            % topleft of screen is 0,0
+            xyTopLeft=S.winCntrXYpx+[S.xPx-S.wPx/2 S.yPx-S.hPx/2];
+            xyBotRite=S.winCntrXYpx+[S.xPx+S.wPx/2 S.yPx+S.hPx/2];
+            rect=[xyTopLeft xyBotRite];
+            Screen('FillRect',S.physScrVals.windowPtr,S.RGBA,rect);
+        end
+    end
+end
