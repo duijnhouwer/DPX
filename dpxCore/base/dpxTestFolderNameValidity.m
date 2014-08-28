8b4da0ddf77054d8e8e0d542dd6d03302def2f0e
j.duijnhouwer@gmail.com <j.duijnhouwer@gmail.com@b6c80056-17eb-79a3-90b5-666747226c3d>
2014-08-28 15:24:40 +0000

    added dpxTestFolderNameValidity.m

---
+++ dpxCore/base/dpxTestFolderNameValidity.m
@@ -0,0 +1,22 @@
+function errstr = dpxTestFolderNameValidity(foldername)
+    
+    % errstr = dpxTestFolderNameValidity(foldername)
+    % EXAMPLE:
+    %   foldername='C:\TESTDIR';
+    %   error(dpxTestFolderNameValidity(foldername)); % error([]) is ignored
+    
+    errstr=[];
+    if ~ischar(foldername)
+        errstr='foldername should be a string (char)';
+    elseif exist(foldername,'file')~=7
+        try
+            mkdir(foldername);
+        catch % me
+            errstr=(['Could not create folder ''' foldername '''.']);
+        end
+        if exist(foldername,'file')==7
+            rmdir(foldername);
+        end
+    end
+end
+
