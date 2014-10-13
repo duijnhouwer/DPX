a75b4adbb5fe1b6cd5ee0f130561b5e426e6bc6c
j.duijnhouwer@gmail.com <j.duijnhouwer@gmail.com@b6c80056-17eb-79a3-90b5-666747226c3d>
2014-10-13 20:27:20 +0000

    added jdToggleFontSizeEditor to toggle the fontsize in the editor, much needed when working for long periods on my laptop screen

---
+++ extra/jdToggleFontSizeEditor.m
@@ -0,0 +1,26 @@
+function jdToggleFontSizeEditor(fontsize)
+    
+    % jdToggleFontSizeEditor(fontsize)
+    %
+    % Change the font size in the Matlab editor window.
+    %
+    % USAGE:
+    %   jdToggleFontSizeEditor [Without argument] switches between Matlab
+    %   default editor-fonts, and larger ones that puts less strain on my
+    %   eyes when I use my ASUS N550jv's laptop.
+    %
+    %   jdToggleFontSizeEditor(fontsize) change size to numerical fontsize.
+    %
+    % Jacob Duijnhouwer, 2014-10-13
+    
+    if nargin==0
+        k=com.mathworks.services.FontPrefs.getCodeFont;
+        if get(k,'Size')==13
+            fontsize=16;
+        else
+            fontsize=13; % default matlab editor font size
+        end
+    end
+    F=java.awt.Font('Courier New',java.awt.Font.PLAIN,fontsize);
+    com.mathworks.services.FontPrefs.setCodeFont(F);
+end
