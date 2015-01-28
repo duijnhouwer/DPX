6e0e38b88115b45554e9f1821dd2ca18198ab03b
e.arani@gmail.com <e.arani@gmail.com@b6c80056-17eb-79a3-90b5-666747226c3d>
2015-01-28 09:50:12 +0000

    noticed tiny error in scopecross

--- dpxExperiments/Reinder/rdDpxExpScopeCross.m
+++ dpxExperiments/Reinder/rdDpxExpScopeCross.m
@@ -2,15 +2,13 @@ function rdDpxExpScopeCross
 %fails :D
 
 E=dpxCoreExperiment;
-E.scr.set('winRectPx',[],'widHeiMm',[394 295],'distMm',1000);
-E.scr.set('stereoMode','mirror','SkipSyncTests',1);
+E.scr.set('winRectPx',[1440 0 1600+1440 1200],'widHeiMm',[394 295],'distMm',1000,'scrNr',1);
+E.scr.set('stereoMode','mirror','skipSyncTests',1);
 C=dpxCoreCondition;
 set(C,'durSec',3600);
 S=dpxStimCross;
 set(S,'wDeg',5,'hDeg',5,'lineWidDeg',0.05,'name','giantfix');
 C.addStim(S);
-R=dpxCoreResponse;
-C.addResp(R);
 E.addCondition(C);
 E.run
 
