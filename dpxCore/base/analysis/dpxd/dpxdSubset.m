8f698fb276e4d44993b87c32c3b03f7633f020fa
duijnhouwer <duijnhouwer@users.noreply.github.com>
2015-09-04 16:05:51 -0400

    Performance upgrade and simplification of dpxdSubset; added plotstuff/dpxXaxis.m and plotstuff/dpxYaxis.m; Bug fixes in jdDpxExpHalfDomeRdkAnalysis.m (contained references to files outside DPX, local to jacobs setup)

--- dpxCore/base/analysis/dpxd/dpxdSubset.m
+++ dpxCore/base/analysis/dpxd/dpxdSubset.m
@@ -1,56 +1,42 @@
-function f=dpxdSubset(r,indices)
+function F=dpxdSubset(R,indices)
 
 % function F=dpxdSubset(R,IDXS) 
-% Of a pdxTbl R, return the subset F corresponding to R at the
+% Of a DPXD R, return the subset F corresponding to R at the
 % given indices. Note that if indices is [], F will not be
 % empty but a complete DPXD with the same, but empty, fields as R
 % and F.N=0.
 
 %%% Handle varargin list%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 p = inputParser;   % Create an instance of the inputParser class.
-p.addRequired('r', @dpxdIs);
+p.addRequired('R', @dpxdIs);
 p.addRequired('indices',@(x)isnumeric(x) | islogical(x));
-p.parse(r,indices);
+p.parse(R,indices);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 if all(islogical(indices))
-    % this function works with whole number indexing, convert logical indexing to natural indexing now,
-    % todo: would be better to do it the other way around becayuse logicla indexing is faster...
+    % this function works with whole number indexing, convert logical indexing to natural
+    % indexing now. TODO: might be better to do it the other way around because logical
+    % indexing is faster...
     indices=find(indices);
 end
-
-
-oldn=r.N;
-if all(islogical(indices)), newn=sum(indices);
-elseif all(dpxIsWholeNumber(indices)), newn=numel(indices);
-else error('Indices should be whole numbers of logical');
+oldn=R.N;
+if all(islogical(indices))
+    newn=sum(indices);
+elseif all(dpxIsWholeNumber(indices))
+    newn=numel(indices);
+else
+    error('Indices should be whole numbers of logical');
 end
 if newn>oldn
-    error(['Number of requested indices (' n ') cannot be larger than the available values in TblStruct (' oldn ').']);
+    error(['Number of requested indices (' num2str(newn) ') cannot be larger than the available values in DPXD (' num2str(oldn) ').']);
 end
-
-
-
-% Remove the special N field. Will be put back (with an updated n
-% value) at the end of this function
-r=rmfield(r,'N');
-
+% Remove the special N field. Will be put back (with an updated value) at the end of this
+% function
+R=rmfield(R,'N');
 % Select the subset of r, store in f
-fn=fieldnames(r);
+fn=fieldnames(R);
 for i=1:length(fn)
-	thisname=fn{i};
-    if iscell(r.(thisname))
-        vals=cell(1,newn);
-        for j=1:newn
-            thisindex=indices(j);
-            vals{j}=r.(thisname){thisindex};
-        end
-        f.(thisname)=vals;
-    else
-        vals=r.(thisname)(indices);
-        f.(thisname)=vals;
-    end
+    F.(fn{i})=R.(fn{i})(indices);
 end
-
 % Put the special N field back in place
-f.N=newn;
+F.N=newn;
