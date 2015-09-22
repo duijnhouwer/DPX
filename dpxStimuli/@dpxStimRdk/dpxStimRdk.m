c0cd85ca91c34cccfa4c0c9baadffb7513cf3721
duijnhouwer <duijnhouwer@users.noreply.github.com>
2015-09-22 00:01:04 -0400

    Modified dpxStimRdk.m so that 50/50 transparency can be used (the separation of the two components in degrees is defined by the imaginary part of dirDeg)

--- dpxStimuli/@dpxStimRdk/dpxStimRdk.m
+++ dpxStimuli/@dpxStimRdk/dpxStimRdk.m
@@ -17,7 +17,7 @@ classdef dpxStimRdk < dpxAbstractVisualStim
         nDots;
         dotXPx;
         dotYPx;
-        dotDirDeg=[];
+        dotDirRads=[];
         dotDiamPx;
         dotAge;
         pxPerFlip; % the speed in pixels per flip
@@ -34,7 +34,7 @@ classdef dpxStimRdk < dpxAbstractVisualStim
             S.dotDiamDeg=.1;
             S.dotRBGAfrac1=[0 0 0 1];
             S.dotRBGAfrac2=[1 1 1 1];
-            S.nSteps=2;
+            S.nSteps=1; % single step is default, use Inf for unlimited
             S.cohereFrac=1; % negative coherence flips directions
             S.apert='circle';
             S.wDeg=10;
@@ -47,26 +47,34 @@ classdef dpxStimRdk < dpxAbstractVisualStim
         function myInit(S)
             % Convert settings to stimulus properties
             S.nDots=max(0,round(S.dotsPerSqrDeg * S.wDeg * S.hDeg));
+            S.nDots=round(S.nDots/2)*2; % make sure even, better for 50/50 split of motion directions for example
             N=S.nDots;
-            S.dotXPx = S.RND.rand(1,N) * S.wPx-S.wPx/2;
-            S.dotYPx = S.RND.rand(1,N) * S.hPx-S.hPx/2;
-            S.dotDirDeg = ones(1,N) * S.dirDeg;
-            nNoiseDots = max(0,min(N,round(N * (1-abs(S.cohereFrac)))));
-            S.noiseDots = [true(1,nNoiseDots) false(1,N-nNoiseDots)];
+            S.dotXPx=S.RND.rand(1,N)*S.wPx-S.wPx/2;
+            S.dotYPx=S.RND.rand(1,N)*S.hPx-S.hPx/2;
+            S.dotDirRads=ones(1,N)*real(S.dirDeg)/180*pi;
+            if ~isreal(S.dirDeg)
+                % imaginary component of S.dirDeg can be used to make half the dots move in
+                % a direction imag(S.dirDeg) away from real(S.dirDeg)
+                S.dotDirRads(1:2:end)=S.dotDirRads(1:2:end)+imag(S.dirDeg)/180*pi;
+            end  
+            nNoiseDots=max(0,min(N,round(N * (1-abs(S.cohereFrac)))));
+            S.noiseDots=[true(1,nNoiseDots) false(1,N-nNoiseDots)];
             S.noiseDots=S.noiseDots(S.RND.randperm(numel(S.noiseDots)));
-            noiseDirs = S.RND.rand(1,N) * 360;
-            S.dotDirDeg(S.noiseDots) = noiseDirs(S.noiseDots);
-            if S.cohereFrac<0, S.dotDirDeg = S.dotDirDeg + 180; end % negative coherence flips directions
+            noiseDirRads=S.RND.rand(1,N)*2*pi;
+            S.dotDirRads(S.noiseDots)=noiseDirRads(S.noiseDots);
+            if S.cohereFrac<0
+                S.dotDirRads=S.dotDirRads+pi; % negative coherence flips directions
+            end 
             [S.dotDiamPx,wasoutofrange]=dpxClip(S.dotDiamDeg*S.scrGets.deg2px,S.scrGets.limits.GL_ALIASED_POINT_SIZE_RANGE);
             if wasoutofrange
                 S.dotDiamDeg=S.dotDiamPx/S.scrGets.deg2px;
                 warning(['S.dotDiamDeg was out of range for this computer, capped at the limit of ' num2str(S.dotDiamDeg) ' degrees.']);
             end
-            S.dotAge = floor(S.RND.rand(1,N) * (S.nSteps + 1));
-            S.pxPerFlip = S.speedDps * S.scrGets.deg2px / S.scrGets.measuredFrameRate;
-            S.dotPolarity = S.RND.rand(1,N)<.5;
-            S.dotsRGBA(:,S.dotPolarity) = repmat(S.dotRBGAfrac1(:)*S.scrGets.whiteIdx,1,sum(S.dotPolarity));
-            S.dotsRGBA(:,~S.dotPolarity) = repmat(S.dotRBGAfrac2(:)*S.scrGets.whiteIdx,1,sum(~S.dotPolarity));
+            S.dotAge=floor(S.RND.rand(1,N) * (S.nSteps + 1));
+            S.pxPerFlip=S.speedDps * S.scrGets.deg2px / S.scrGets.measuredFrameRate;
+            S.dotPolarity=S.RND.rand(1,N)<.5;
+            S.dotsRGBA(:,S.dotPolarity)=repmat(S.dotRBGAfrac1(:)*S.scrGets.whiteIdx,1,sum(S.dotPolarity));
+            S.dotsRGBA(:,~S.dotPolarity)=repmat(S.dotRBGAfrac2(:)*S.scrGets.whiteIdx,1,sum(~S.dotPolarity));
         end
         function myDraw(S)
             if S.visible
@@ -87,33 +95,30 @@ classdef dpxStimRdk < dpxAbstractVisualStim
                 y=S.dotYPx;
                 w=S.wPx;
                 h=S.hPx;
-                dx=cosd(S.dotDirDeg)*S.pxPerFlip*S.freezeFlip;
-                dy=sind(S.dotDirDeg)*S.pxPerFlip*S.freezeFlip;
                 % Update dot lifetime
                 S.dotAge=S.dotAge+1;
                 expired=S.dotAge>S.nSteps;
                 % give new position if expired
-                x(expired)=S.RND.rand(1,sum(expired))*w-w/2-dx(expired);
-                y(expired)=S.RND.rand(1,sum(expired))*h-h/2-dy(expired);
+                x(expired)=S.RND.rand(1,sum(expired))*w-w/2;
+                y(expired)=S.RND.rand(1,sum(expired))*h-h/2;
                 % give new random direction if expired and dot is noise
-                rndDirs=S.RND.rand(size(x))*360;
-                S.dotDirDeg(expired&S.noiseDots)=rndDirs(expired&S.noiseDots);
+                S.dotDirRads(expired&S.noiseDots)=S.RND.rand(1,sum(expired&S.noiseDots))*2*pi;
                 S.dotAge(expired)=0;
-                % Move the dots
-                x=x+dx;
-                y=y+dy;
-                if dx>0
-                    x(x>=w/2)=x(x>=w/2)-w;
-                elseif dx<0
-                    x(x<-w/2)=x(x<-w/2)+w;
-                end
-                if dy>0
-                    y(y>=h/2)=y(y>=h/2)-h;
-                elseif dy<0
-                    y(y<-h/2)=y(y<-h/2)+h;
-                end
+                % Move the dots, note, dots that have just been replaced within the
+                % aperture because of life-time expirations should not step
+                dx=cos(S.dotDirRads(~expired))*S.pxPerFlip*S.freezeFlip;
+                dy=sin(S.dotDirRads(~expired))*S.pxPerFlip*S.freezeFlip;
+                x(~expired)=x(~expired)+dx;
+                y(~expired)=y(~expired)+dy;
+                % Wrap the dots around if they cross the stimulus edge
+                x(x>=w/2)=x(x>=w/2)-w;
+                x(x<-w/2)=x(x<-w/2)+w;
+                y(y>=h/2)=y(y>=h/2)-h;
+                y(y<-h/2)=y(y<-h/2)+h;
+                % Copy shorthand into member variables
                 S.dotXPx=x;
                 S.dotYPx=y;
+                % Flip the contrasts in case of reverse-phi motion
                 if strcmpi(S.motType,'IHP')
                     % Flip rgba1 and rgba2
                     rgba1=S.dotRBGAfrac1(:)*S.scrGets.whiteIdx;
