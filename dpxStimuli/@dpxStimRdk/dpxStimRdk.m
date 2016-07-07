92674e5bb4a2bb5970d5b442f19bc1dd8af113be
duijnhouwer <duijnhouwer@users.noreply.github.com>
2016-07-07 09:15:55 -0400

    "lenient" option from Screen('DrawDots') removed in dpxStimRdk.m This did not work on the 2photon experiment computer. Seems like its a recent addition to Psychtoolbox that is not on the computers (Psychtoolbox could use an update on those machines)

--- dpxStimuli/@dpxStimRdk/dpxStimRdk.m
+++ dpxStimuli/@dpxStimRdk/dpxStimRdk.m
@@ -89,7 +89,7 @@ classdef dpxStimRdk < dpxAbstractVisualStim
                 ok=applyTheAperture(S);
                 if ~any(ok), return; end
                 xy=[S.dotXPx(:)+S.xPx S.dotYPx(:)+S.yPx]';
-                Screen('DrawDots',S.scrGets.windowPtr,xy(:,ok),S.dotDiamPx,S.dotsRGBA(:,ok),S.winCntrXYpx,2,1);
+                Screen('DrawDots',S.scrGets.windowPtr,xy(:,ok),S.dotDiamPx,S.dotsRGBA(:,ok),S.winCntrXYpx,2);
             end
         end
         function myStep(S)
