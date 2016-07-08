05128eaa37f1e2f79833089f2cc6859c77a09d97
duijnhouwer <duijnhouwer@users.noreply.github.com>
2016-07-08 11:32:00 -0400

    update to lkFrameDropsPerCondition.m (see previous commit)

--- dpxExperiments/Laurens/lkFrameDropsPerCondition.m
+++ dpxExperiments/Laurens/lkFrameDropsPerCondition.m
@@ -4,6 +4,13 @@ function lkFrameDropsPerCondition(DPXD,var)
     % load('C:\Users\jacob\Downloads\lkDpxTuningSpeedDotDiamRdk0-DUMMYJD-20160708163319.mat')
     % lkFrameDropsPerCondition(DPXD,'test_dotDiamDeg');
     
+    dpxFindFig(mfilename);
+    subplot(1,2,1,'align');
+    hist(DPXD.nrMissedFlips);
+    xlabel('nrMissedFlips');
+    ylabel('% trials');
+    title('Across all cond''s');
+    % 
     DPXD.framesPerTrial=(DPXD.stopSec-DPXD.startSec)*DPXD.window_measuredFrameRate(1);
     D=dpxdSplit(DPXD,var);
     meanPctFrameDrops=nan(size(D));
@@ -13,6 +20,9 @@ function lkFrameDropsPerCondition(DPXD,var)
         meanPctFrameDrops(i)=mean(percentmissed);
         stdPctFrameDrops(i)=std(percentmissed);
     end
+    subplot(1,2,1,'align');
+    hist(DPXD.nrMissedFlips);
+    subplot(1,2,2,'align');
     errorbar(meanPctFrameDrops,stdPctFrameDrops,'.-');
     var(var=='_')='.';
     xlabel(sprintf('%s condition #',var));
