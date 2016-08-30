6c4626a34b93aa69c87fc704ff691412093b2b4c
reinderdorman <reind.dorman@gmail.com>
2016-08-30 16:45:38 +0200

    kinda finished a main and relatively complete anaylsis

--- dpxExperiments/Reinder/Analysis/rdAnalyseCylinderDepth.m
+++ dpxExperiments/Reinder/Analysis/rdAnalyseCylinderDepth.m
@@ -14,22 +14,38 @@ function  rdAnalyseCylinderDepth(varargin)
 %           Optional data input. if not included, will
 %           prompt a loading screening in which you can select
 %           whichever files you want to load.
-
+%
 %       Average ('average',1)
 %           true or false, default is true.
 %           Average the data or plot all subjects individually. Just pass a
 %           logical true or false.
 %
-% OUTPUT
-%   not yet
+%       Disps ('disps',[-.8 -.4 0 .4 .8])
+%           only plotting specific disparities. This is usefull when
+%           analyzing datafiles in which datasets have discrepancies
+%           between disparities, for instance, some sets have disparities
+%           ranging from -1 to 1, some -.8 to .8. By default, this function
+%           interpolates such cases, but if you want to have less data
+%           manipulation, you can use this input to define the plotted
+%           disparities.
 %
-% ATM: loop through the subjects.
+% OUTPUT
+%   N.A., only figures.
+%
+% FOOTNOTE
+%   Last edit by Reinder Dorman, augustus 2016, for Biophysics department
+%   Radboud University
+%
+%   Questions and comments: reind.dorman(at)gmail.com
+%
 
 
 %% Assess input
 P=parsePairs(varargin);
 checkField(P,'data',[]);
 checkField(P,'average',1);
+checkField(P,'disps',[]); % INSERT TO PICK A SPECIFIC DISP
+
 
 %% Loading and checking
 if isempty(P.data);
@@ -50,7 +66,8 @@ if numel(unique(subjects))>1;
     uSubjects = unique(subjects);
     fprintf('Loading Multiple subjects:')
     for iSubjects = 1:numel(uSubjects);
-        fprintf(' %s - ',uSubjects{iSubjects});
+        if iSubjects>1 fprintf(' - '); end
+        fprintf(' %s ',uSubjects{iSubjects});
     end
     fprintf('\n');
 end
@@ -61,9 +78,22 @@ end
 
 %% Some pre-processing
 for d=1:numel(Data)
+    % check if files are really DPX data files
     if ~dpxdIs(Data{d}) || Data{d}.N<=1;
         warning(['Removing non-dpx file: ' fnames{d}]);
         Data{d}=[];
+    end
+end
+
+if ~isempty(P.disps)
+    fprintf('Selected disparities : %s\n',num2str([P.disps])');
+    for d=1:numel(Data)
+        % check for availability of chosen disparities.
+        if ~all(ismember(P.disps,Data{d}.halfInducerCyl_disparityFrac))
+            warning(['Removing file with non-corresponding disparity values: ' fnames{d}]);
+            fprintf('\t Disparities in data: %s\n',num2str(unique(Data{d}.halfInducerCyl_disparityFrac)));
+            Data{d}=[];
+        end 
     end
 end
 
@@ -74,8 +104,8 @@ fprintf('Discarded %d out of %d trials for lack of response\n',oldN-Data.N,oldN)
 
 Data = dpxdSubset(Data,Data.halfInducerCyl_stereoLumCorr==1); 
 
-% check first trial is removed, the adaptation trial.
-% should already be cleared by the Discard lack of response part.
+% Check first trial is removed, the adaptation trial.
+% Should already be cleared by the Discard lack of response part.
 Data = dpxdSubset(Data,Data.halfInducerCyl_rotSpeedDeg~=0);
 
 %% Process Data
@@ -87,7 +117,12 @@ for s = 1:numel(subjects)
     if strcmp(Data{s}.exp_paradigm{1},'rdDpxExpAdaptDepth_diep') || strcmp(Data{s}.exp_paradigm{1},'rdDpxExpBaseLineCylLeft');
         %%% ANALYZE DEPTH PERCEPT DATA
         
-        totalDisparities = unique(Data{s}.halfInducerCyl_disparityFrac);
+        if ~isempty(P.disps)
+            totalDisparities = P.disps;
+        else
+            totalDisparities = unique(Data{s}.halfInducerCyl_disparityFrac);
+        end
+        
         for iDisp = 1:numel(totalDisparities)
             iData = dpxdSubset(Data{s},Data{s}.halfInducerCyl_disparityFrac==totalDisparities(iDisp));
             x{s}(iDisp) = mean(iData.halfInducerCyl_disparityFrac);
@@ -97,9 +132,16 @@ for s = 1:numel(subjects)
     elseif strcmp(Data{s}.exp_paradigm{1},'rdDpxExpAdaptDepth_bind') || strcmp(Data{s}.exp_paradigm{1},'rdDpxExpBindingCylLeft')
         %%% ANALYZE BINDING DATA
         
-        totalDisparities    = unique(Data{s}.halfInducerCyl_disparityFrac);
-        speeds              = unique(Data{s}.halfInducerCyl_rotSpeedDeg);
+        if ~isempty(P.disps)
+            totalDisparities = P.disps;
+        else
+            totalDisparities = unique(Data{s}.halfInducerCyl_disparityFrac);
+        end
+        speeds               = unique(Data{s}.halfInducerCyl_rotSpeedDeg);
         % unique sorts lowest to highest.
+        
+        totalDisparities(totalDisparities==0)=[]; 
+        % zero disparity makes no sense in binding, removed.
         
         x(s,:) = [min(totalDisparities) max(totalDisparities)];
         
@@ -130,14 +172,26 @@ for s = 1:numel(subjects)
 end
 
 if iscell(x) && any(diff(cellfun(@numel,x))~=0) && P.average
-    warning('Uneven disparities between subjects. Interpolating for averaging');
+    warning('Uneven disparities between subjects. Interpolating for averaging');          
+    fprintf('To plot only specific disparities, use the ''disps'' input\n\t See ');
+    fprintf('<a href="matlab: help rdAnalyseCylinderDepth">help</a> for more info.\n');
+
     xInt = -1:.2:1;
     for s = 1:numel(subjects)
         yInt(s,:) = interp1(x{s},y{s},xInt);
     end
+    x = xInt;
+    y = yInt;
+    yAvg = nanmean(yInt,1);
+    yStd = nanstd(yInt,0,1);
 elseif iscell(x) && any(diff(cellfun(@numel,x))~=0)
     warning('Uneven disparities between subjects.')
     maxDisps = unique(max(cellfun(@numel,x)));
+elseif iscell(x)
+    x = cell2mat(x');
+    y = cell2mat(y');
+    yStd = std(y,0,1);
+    yAvg = mean(y,1);
 else
     yStd = std(y,0,1);
     yAvg = mean(y,1);
@@ -145,13 +199,13 @@ end
 
 
 
-%% statistics
+%% STATISTICS
 % paired t-test because simply estimate differences between means of two
 % experiments on the same subjects
 
 [H,p] = ttest(y(:,1),y(:,2));
 
-%% display
+%% DISPLAY
 if P.average % average or loose plots?
     x = mean(x,1);
     y = yAvg;
@@ -179,9 +233,9 @@ if strcmp(Data{s}.exp_paradigm{1},'rdDpxExpAdaptDepth_diep') || strcmp(Data{s}.e
     
     plot(get(gca,'XLim'),[.5 .5],'--','Color',[.5 .5 .5]);
     plot([0 0],[0 1],'--','Color',[.5 .5 .5]);
-    ylim([0 1]);
+    ylim([0 1.1]);
     
-    xlabel('Disparities');
+    xlabel('Disparity fractions');
     ylabel('Percentage Convex');
     
 elseif strcmp(Data{s}.exp_paradigm{1},'rdDpxExpAdaptDepth_bind') || strcmp(Data{s}.exp_paradigm{1},'rdDpxExpBindingCylLeft');
@@ -194,7 +248,8 @@ elseif strcmp(Data{s}.exp_paradigm{1},'rdDpxExpAdaptDepth_bind') || strcmp(Data{
         
         plot(get(gca,'XLim'),[.5 .5],'--','Color',[.5 .5 .5]);
         plot([0 0],[0 1],'--','Color',[.5 .5 .5]);
-        ylim([0 1]);
+        set(gca,'XTick',totalDisparities);
+        ylim([0 1.1]);
     else
         for s = 1:nSubjects;
             subplot(ceil(nSubjects/4),4,s),
@@ -208,7 +263,13 @@ elseif strcmp(Data{s}.exp_paradigm{1},'rdDpxExpAdaptDepth_bind') || strcmp(Data{
         end
         
     end
+    
+    xlabel('Disparity fractions');
+    ylabel('Percentage Bound');
+    
     if p<0.05 ;
+        % if significant P value, make a significance asterisk and bar on
+        % the plot.
         hold on
         intervalX=[-1 -1 1 1 ];
         intervalY=[max(yAvg)+max(yStd)+0.1 max(yAvg)+max(yStd)+0.15 max(yAvg)+max(yStd)+0.15 max(yAvg)+max(yStd)+0.1];
@@ -218,10 +279,7 @@ elseif strcmp(Data{s}.exp_paradigm{1},'rdDpxExpAdaptDepth_bind') || strcmp(Data{
     end
 end
 
-
 hold on axis
-
-
 
 if P.average rdGraphText(['N = ' num2str(numel(unique(subjects)))],'northwest'); end;
 
